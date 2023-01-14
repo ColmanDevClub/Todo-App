8638dd1d6730844c342c02d87f88f986e57407a4
Aviv Nataf <118442152+an1604@users.noreply.github.com>
2023-01-14 21:26:45 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { useState } from 'react';
-import './App.css';
-import Header from './components/Header';
-import TodoList from './components/TodoList';
+import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const [todos,setTodos]=useState([]);
+  const [todos, setTodos] = useState([]);
 
-  return (<div>
-    <Header setTodos={setTodos} />
-    <TodoList todos={todos} setTodos={setTodos} />
-  </div>);
+  return (
+    <div>
+      <Header setTodos={setTodos} />
+      <TodoList todos={todos} setTodos={setTodos} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,32 +1,28 @@
-import React, { useState } from 'react'
-
+import React, { useState } from "react";
+import TodoItem from "./TodoItem";
 export default function TodoList({ todos, setTodos }) {
-    const markAsCompleted = (taskId) => {
-        const updatedTodos = todos.map((todo) => {
-            if (todo.id === taskId) {
-                return {...todo, isCompleted: !todo.isCompleted};
-            }
-            return todo;
-        });
-        setTodos(updatedTodos);
-    };
+  const markAsCompleted = (taskId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === taskId) {
+        return { ...todo, isCompleted: !todo.isCompleted };
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
 
   return (
-    <ul className='todoItems'>
-        {todos.map(item => {
-            return (
-                <div className='control-group'>
-                    <label className='control-control-group'>
-                <li key={item.id} className='items' >
-                    <input  id='check' type='checkbox' checked={item.isCompleted} onChange={()=>markAsCompleted(item.id)}/>
-                    {item.content}
-                </li>
-                </label>
-                </div>
-            )
-        })}
+    <ul className="todoItems">
+      {todos.map((item) => {
+        return (
+          <TodoItem
+            key={item.id}
+            content={item.content}
+            id={item.id}
+            isCompleted={item.isCompleted}
+          />
+        );
+      })}
     </ul>
-  )
+  );
 }
-
-
